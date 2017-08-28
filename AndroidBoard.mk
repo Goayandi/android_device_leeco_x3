73ec4571ab4655bc98cd6314f8385dba7c6469f1
vishalk17 <vishalkapadi95@gmail.com>
2017-08-28 05:51:11 -0700

    build kernel with rom

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,7 +1,16 @@
-LOCAL_PATH := $(call my-dir)
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+KERNEL_DEFCONFIG := x500-debug_defconfig
+else
+KERNEL_DEFCONFIG := x500_defconfig
+endif
+KERNEL_DIR := kernel/LeEco/X3
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
-include $(CLEAR_VARS)
+include $(TOP)/$(KERNEL_DIR)/Android.mk
 
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
-
-# include the non-open-source counterpart to this file
+# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
+# So cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel

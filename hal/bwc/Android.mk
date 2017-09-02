852d00167e1aaa57405096a46ba2593ceabe93e3
vishalk17 <vishalkapadi95@gmail.com>
2017-09-02 12:48:46 +0530

    Trying to fix C_INCLUDE ISSUE #1

--- hal/bwc/Android.mk
+++ hal/bwc/Android.mk
@@ -70,7 +70,7 @@ LOCAL_SRC_FILES:= \
     
 
 LOCAL_C_INCLUDES:= \
-/inc 
+#/inc 
   #$(TOP)/frameworks/base/include/ \
   #$(MTK_PATH_SOURCE)/hardware/bwc/inc \
   #$(TOP)/$(MTK_PATH_PLATFORM)/kernel/core/include/mach \

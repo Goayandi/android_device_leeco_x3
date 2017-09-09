494708c1b6f8f488003569a0b59f166c2fbe3a19
vishalk17 <vishalkapadi95@gmail.com>
2017-09-09 13:54:58 +0530

    Move vendor to lineage

--- lineage_X3.mk
+++ lineage_X3.mk
@@ -1,8 +1,11 @@
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/LeEco/X3/full_X3.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1920
@@ -16,6 +19,12 @@ PRODUCT_MANUFACTURER := LeEco
 PRODUCT_RELEASE_NAME := X3
 PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_MODEL := Le 1S
+
+#Maintainer Name
+PRODUCT_PROPERTY_OVERRIDES += ro.lineage.maintainer="vishalk17"
+
+#Don't Ship SuperSU
+WITH_SUPERSU := false
 
 # SDcard
 PRODUCT_CHARACTERISTICS := nosdcard

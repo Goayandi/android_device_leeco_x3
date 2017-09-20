90ef19df3eb8e11144e15baca5eb23fbcf8d4ec8
vishalk17 <vishalkapadi95@gmail.com>
2017-09-20 05:11:32 -0700

    Update extra.mk

--- mediatek/extra.mk
+++ mediatek/extra.mk
@@ -1,7 +1,16 @@
 #Let me define some modifications
 MTK_TARGET_PROJECT := X3
-
+MTK_PLATFORM := MT6795
+MTK_BASE_PROJECT := $(MTK_TARGET_PROJECT)
+MTK_PROJECT_FOLDER := $(shell find device/* -maxdepth 1 -name $(MTK_BASE_PROJECT))
+MTK_TARGET_PROJECT_FOLDER := $(shell find device/* -maxdepth 1 -name $(MTK_TARGET_PROJECT))
 
 # Use the 6795 common part
 include device/mediatek/mt6795/BoardConfig.mk
+
+#Project_configuration , This file may contain all configurations which we want.
+include device/LeEco/X3/ProjectConfig.mk
+
+
+
 

c42e3c68166c566e3822f2106602538384d70ba6
vishalk17 <vishalkapadi95@gmail.com>
2017-09-21 20:24:11 -0700

    Update lineage_X3.mk

--- lineage_X3.mk
+++ lineage_X3.mk
@@ -1,27 +1,31 @@
-# Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Device display
-TARGET_SCREEN_HEIGHT := 1920
+# Boot animation
 TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+# Inherit device configuration
+$(call inherit-product, device/LeEco/X3/full_X3.mk)
+
+# Inherit some common cm stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X3
 PRODUCT_NAME := lineage_X3
 PRODUCT_BRAND := LeEco
+PRODUCT_MODEL := LeEco Le 1s
 PRODUCT_MANUFACTURER := LeEco
-PRODUCT_RELEASE_NAME := X3
-PRODUCT_RESTRICT_VENDOR_FILES := false
-PRODUCT_MODEL := Le 1S
 
-# SDcard
-PRODUCT_CHARACTERISTICS := nosdcard
+#Don't Ship SuperSU
+WITH_SUPERSU := false
+
+#Maintainer Name
+PRODUCT_PROPERTY_OVERRIDES += ro.cm.maintainer="vishalk17"
+
+# Release name
+PRODUCT_RELEASE_NAME := X3
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Letv/Le1s_CN/X3:6.0/DBXCNOP5902302082S/1486532099:user/release-keys 
+    BUILD_FINGERPRINT=Letv/Le1s_CN/X3:6.0/DBXCNOP5902302082S/1486532099:user/release-keys
 
 
 

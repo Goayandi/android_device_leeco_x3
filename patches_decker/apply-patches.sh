c952023be701478d8b83826b7be5befe492eb59c
vishalk17 <vishalkapadi95@gmail.com>
2017-09-27 04:11:06 -0700

    Update apply-patches.sh

--- patches_decker/apply-patches.sh
+++ patches_decker/apply-patches.sh
@@ -2,7 +2,7 @@
 cd ../../../..
 cd system/core
 git apply -v ../../device/LeEco/X3/patches_decker/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-git apply -v ../../device/LeEco/X3/patches_decker/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
+#remaining" git apply -v ../../device/LeEco/X3/patches_decker/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
 git apply -v ../../device/LeEco/X3/patches_decker/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
 cd ../..
 cd bionic
@@ -16,7 +16,7 @@ git apply -v ../../../device/LeEco/X3/patches_decker/0005-add-author-info-in-dev
 cd ../../..
 cd frameworks/av
 git apply -v ../../device/LeEco/X3/patches_decker/0003-add-mising-MediaBufferGroup-acquire_buffer-symbol-2.patch
-git apply -v ../../device/LeEco/X3/patches_decker/0006-fix-access-wvm-to-ReadOptions.patch
+#doesnt need to patch# git apply -v ../../device/LeEco/X3/patches_decker/0006-fix-access-wvm-to-ReadOptions.patch
 git apply -v ../../device/LeEco/X3/patches_decker/0007-Disable-usage-of-get_capture_position.patch
 git apply -v ../../device/LeEco/X3/patches_decker/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
 git apply -v ../../device/LeEco/X3/patches_decker/0009-add-mtk-color-format-support.patch
@@ -25,5 +25,5 @@ cd system/netd
 git apply -v ../../device/LeEco/X3/patches_decker/0010-wifi-tethering-fix.patch
 cd ../..
 cd frameworks/base
-git apply -v ../../device/LeEco/X3/patches_decker/0013-PackageManager-don-t-delete-data-app-mcRegistry-fold.patch
+#git apply -v ../../device/LeEco/X3/patches_decker/0013-PackageManager-don-t-delete-data-app-mcRegistry-fold.patch
 cd ../..

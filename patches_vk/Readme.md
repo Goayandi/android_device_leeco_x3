5eebd007cb1466457753ef01370d24a53bd3e208
vishalk17 <vishalkapadi95@gmail.com>
2017-10-01 11:51:43 -0700

    new patches

---
+++ patches_vk/Readme.md
@@ -0,0 +1,13 @@
+My all patches are highly experimental.
+
+##How to apply patches.??????
+
+just head over to this directory ; patches_vk
+
+In terminal 
+
+type below commond and hit enter 
+
+sh patches_vk.sh
+
+now go back in rom source directory then build ur rom.
---
+++ patches_vk/patches_oreo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ../../../..
+cd system/core
+git apply -v ../../device/LeEco/X3/patches_vk/system/core/system_core.patch
+git apply -v ../../device/LeEco/X3/patches_vk/system/core/0001-mtk-O-reintroduce-atomic-symbols-in-libcutils-for-ol.patch
+cd ../..
+cd bionic
+git apply -v ../device/LeEco/X3/patches_vk/bionic/bionic.patch
+cd ..
+cd frameworks/av
+git apply -v ../../device/LeEco/X3/patches_vk/frameworks/av/frameworks_av.patch
+git apply -v ../../device/LeEco/X3/patches_vk/frameworks/av/frameworks_av_2.patch
+cd ../..
+cd build/soong
+git apply -v ../../device/LeEco/X3/patches_vk/build/soong/0001-build-soong-add-BOARD_USES_MTK_HARDWARE.patch
+cd ../..
+cd build/core
+git apply -v ../../device/LeEco/X3/patches_vk/build/make/core/0001-build-core-add-BOARD_USES_MTK_HARDWARE.patch
+cd ../..

cb8d83b2edc24c60f1374c3bd2f9051f55981549
vishalk17 <vishalkapadi95@gmail.com>
2017-10-10 17:38:06 +0000

    back to the common_full_phone.mk

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 1920
 $(call inherit-product, device/LeEco/X3/full_X3.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X3

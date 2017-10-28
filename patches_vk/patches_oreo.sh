17050ab89a959effc329273d5a54ed3e5255e974
vishalk17 <vishalkapadi95@gmail.com>
2017-10-28 07:01:56 -0700

    patches_oreo.sh: LeEco >> leeco , X3 >> x3

--- patches_vk/patches_oreo.sh
+++ patches_vk/patches_oreo.sh
@@ -15,8 +15,8 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
         echo -e "\n${RED}Applying ${NC}$dir ${RED}patches...${NC}\n"
-	#git am $rootdirectory/device/LeEco/X3/$dir/*.patch
-	git apply -v $rootdirectory/device/LeEco/X3/patches_vk/$dir/*.patch
+	#git am $rootdirectory/device/leeco/x3/$dir/*.patch
+	git apply -v $rootdirectory/device/leeco/x3/patches_vk/$dir/*.patch
 done
 
 # -----------------------------------
